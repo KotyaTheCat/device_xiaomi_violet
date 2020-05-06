310349166081ebcab2abacae721a069252f9c5cd
KotyaTheCat <kotyathecat7@gmail.com>
2020-05-06 11:55:51 +0300

    violet: Build more camera packages

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.common@1.0 \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Context Hub
